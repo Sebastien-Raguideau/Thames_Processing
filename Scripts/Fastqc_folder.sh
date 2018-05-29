db585887fc613b40b2f46b98254a0e1734afde33
Sebastien-Raguideau <Sebastien-Raguideau@users.noreply.github.com>
2018-05-29 18:19:56 +0100

    Update Fastqc_folder.sh

--- Scripts/Fastqc_folder.sh
+++ Scripts/Fastqc_folder.sh
@@ -1,15 +1,16 @@
 #!bin/bash
-# $1 is the folder where all fastq report are stored
+# $1 is the folder where all data are stored
+# A new folder called Quality_Check, will be generated in folder $1
 # Multiqc report will be generated in that folder too
 # $2 is the number of parrallels 
 
 for file in $1/*fastq.gz
 do 
-fastqc $file -o quality_check &
+fastqc $file -o $1/Quality_Check &
 while [[ $(ps aux |grep $USER |grep "[f]astqc"|wc -l) -ge $2 ]]
 do
 	sleep 1m
 done
 done
 
-multiqc --interactive -f $1 -o $1
+multiqc --interactive -f $1/Quality_Check -o $1/Quality_Check
